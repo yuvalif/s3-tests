228e0caf348a180ae5ee7b7fa65fa6316ea86eb2
Yuval Lifshitz <ylifshit@ibm.com>
2025-05-13 16:48:35 +0000

    rgw/logging: test part upload in mpu in standard mode
    Signed-off-by: Yuval Lifshitz <ylifshit@ibm.com>

--- s3tests_boto3/functional/test_s3.py
+++ s3tests_boto3/functional/test_s3.py
@@ -16445,6 +16445,12 @@ def _bucket_logging_mpu(versioned):
     body = _get_body(response)
     record_type = 'Standard' if not has_extensions else 'Journal'
     assert _verify_records(body, src_bucket_name, 'REST.POST.UPLOAD', [src_key, src_key], record_type, expected_count)
+    if record_type == 'Standard':
+        if versioned:
+            expected_count = 12
+        else:
+            expected_count = 6
+        assert _verify_records(body, src_bucket_name, 'REST.PUT.PART', [src_key, src_key], record_type, expected_count)
 
 
 @pytest.mark.bucket_logging
